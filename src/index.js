00800b33d2a95e5f402a82559171855150969af3
jaywyawhare <2005441@kiit.ac.in>
2024-11-30 11:58:33 +0530

    Fix : Minor patches to make it work.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
+require('dotenv').config();
+
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const dotenv = require('dotenv');
 const { createTable, fillUsers } = require('./db/initDB');
 
-const { betWinner } = require('./commands/betWinner');
-const { claim } = require('./commands/claim');
-const { help } = require('./commands/help');
-const { intro } = require('./commands/intro');
-const { leaderboard } = require('./commands/leaderboard');
-const { liveRating } = require('./commands/liveRating');
-const { meme } = require('./commands/meme');
-const { menu } = require('./commands/menu');
-const { odds } = require('./commands/odds');
-const { ping } = require('./commands/ping');
-const { refill } = require('./commands/refill');
-const { stickers } = require('./commands/stickers');
-const { unixporn } = require('./commands/unixporn');
-
-dotenv.config();
+const help = require('./commands/help');
+const intro = require('./commands/intro');
+const meme = require('./commands/meme');
+const menu = require('./commands/menu');
+const odds = require('./commands/odds');
+const ping = require('./commands/ping');
+const unixporn = require('./commands/unixporn');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
@@ -41,68 +34,35 @@ client.on('message', async (message) => {
         const senderId = message.from;
         const allowedUserId = process.env.ALLOWED_USER_ID;
 
-        if (senderId === allowedUserId) {
-            try {
-                switch (command) {
-                    case 'betwinner':
-                        await betWinner(message, args);
-                        break;
-                    case 'claim':
-                        await claim(message, args);
-                        break;
-                    case 'help':   
-                        await help(message, args);
-                        break;
-                    case 'intro':
-                        await intro(message, args);
-                        break;
-                    case 'leaderboard':
-                        await leaderboard(message, args);
-                        break;
-                    case 'liverating':
-                        await liveRating(message, args);
-                        break;
-                    case 'meme':
-                        await meme(message, args);
-                        break;
-                    case 'menu':
-                        await menu(message, args);
-                        break;
-                    case 'odds':
-                        await odds(message, args);
-                        break;
-                    case 'ping':
-                        await ping(message, args);
-                        break;
-                    case 'refill':
-                        await refill(message, args);
-                        break;
-                    case 'stickers':
-                        await stickers(message, args);
-                        break;
-                    case 'unixporn':
-                        await unixporn(message, args);
-                        break;
-                    case 'betachat': 
-                        if (senderId === allowedUserId) {
-                            const chat = await message.getChat();
-                            const participants = chat.participants;
-                            fillUsers(participants); 
-                            message.reply('All group members have been added to the database!');
-                        }
-                        else {
-                            message.reply('You do not have permission to use this command.');
-                        }
-                        break;
-                    default:
-                        await message.reply('Invalid command. Please use =>menu to see available commands.');
-                }
-            } catch (error) {
-                console.error('Error handling command:', error);
-                await message.reply('An error occurred while processing your request.');
+        try {
+            switch (command) {
+                case 'help':   
+                    await help.execute(message, args);
+                    break;
+                case 'intro':
+                    await intro.execute(message, args); 
+                    break;
+                case 'meme':
+                    await meme.execute(message, args);
+                    break;
+                case 'menu':
+                    await menu.execute(message, args);
+                    break;
+                case 'odds':
+                    await odds.execute(message, args);
+                    break;
+                case 'ping':
+                    await ping.execute(message, args);
+                    break;
+                case 'unixporn':
+                    await unixporn.execute(message, args);
+                    break;
+                default:
+                    await message.reply('Invalid command. Please use =>menu to see available commands.');
             }
-        } else {
-            message.reply(`You do not have permission to use this command. Only the authorized user can execute this.`);
+        } catch (error) {
+            console.error('Error handling command:', error);
+            await message.reply('An error occurred while processing your request.');
         }
     }
 });
