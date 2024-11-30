288730b0236f7ee19c1355209cac179f1aa9ee23
jaywyawhare <2005441@kiit.ac.in>
2024-11-30 05:56:52 +0530

    Feat : Small sanity check added

---
+++ src/commands/ping.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'ping',
+    description: 'Checks if the bot is responsive',
+    execute(message, args) {
+        message.reply('Pong! 🏓 The bot is online and active.');
+    }
+};
+
+
